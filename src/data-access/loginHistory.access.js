863a777b7c18f7bb862c0e39893104884a4e481a
spyrot <spyrot@gmail.com>
2021-08-17 00:33:31 +0300

    added limit to login history

--- src/data-access/loginHistory.access.js
+++ src/data-access/loginHistory.access.js
@@ -1,11 +1,11 @@
 const loginRec = require('../models/loginRec.model');
 
 async function post(userName, date) {
-    return await loginRec.updateOne({ _id: userName }, { $push: { dates: date } },  { upsert: true});
+    return await loginRec.updateOne({ _id: userName }, { $push: { dates: date } }, { upsert: true });
 }
 
 async function get(userName) {
-    return await loginRec.findOne({ _id: userName });
+    return await loginRec.findOne({ _id: userName }, { dates: { '$slice': -30 } });
 }
 
 module.exports = {
